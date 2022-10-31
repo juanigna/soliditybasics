e00b7f00f1b914106d1c1e45e2d5277109527dd7
Juan Ignacio <jibocchi14@gmail.com>
2022-10-31 15:20:48 -0300

    Ejericios 1 y 2

--- 1-helloSolidity.sol
+++ 1-helloSolidity.sol
@@ -1,3 +1,12 @@
 /*
 Deberas hacer print del string 'Hello Solidity' en el contrato HelloWorld. Usa str como una variable para almacenar valor
 */
+pragma solidity ^0.8.6;
+
+contract HelloWorld{
+    string public word = "Hello World";
+
+    function getWord() public view returns(string memory){
+        return word;
+    }
+}
--- 2-variableslocalystate.sol
+++ 2-variableslocalystate.sol
@@ -2,3 +2,19 @@
 Deberas crear una state variable e iniciarlizarla con 10 y luego deberias crear una funcion returnStateVariable() que devuelva el valor de la statevariable creada.
 De igual manera, tenes que crear una variable local e iniciarla con 20 y luego tenes que crear una funcion returnLocalVariable que devuelva el valor de la variable local creada. 
 */
+pragma solidity ^0.8.6;
+
+contract Ejercicio2{
+    uint public stateVarible = 10;
+    uint localVariable = 20;
+
+
+   function returnStateVariable() public view returns(uint){
+       return stateVarible;
+   }
+
+
+   function returnLocalVariable() public view returns(uint){
+       return localVariable;
+   }
+}

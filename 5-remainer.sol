04549e7fdcbea87c9ad9df26f73e13a61347507e
Juan Ignacio <jibocchi14@gmail.com>
2022-10-31 16:12:16 -0300

    Ejercicio 6

--- 5-remainer.sol
+++ 5-remainer.sol
@@ -1,3 +1,16 @@
 /* Crea una funcion find(int a) donde a>0;
 Devolve el remainder cuando a es dividido por 3. 
 La funcion debe ser public. */
+
+pragma solidity ^0.8.6;
+
+contract Ejercicio5{
+
+    function find(uint a) public pure returns(uint){
+        if(a>0){
+            uint resto =a%3;
+            return  resto;
+        }
+    }
+   
+}
--- 6-promedio.sol
+++ 6-promedio.sol
@@ -2,3 +2,15 @@
 Crea una funcion promedio(int a, int b, int c)
 Donde a>0, b>0, c>0.
 Devolve el promedio de a, b y c. La funcion debe ser public. */
+
+pragma solidity ^0.8.6;
+
+contract Ejercicio6{
+
+    function promedio(uint a, uint b, uint c) public pure returns(uint){
+        if(a>0 && b>0 && c>0){
+            uint promedioResultado = (a+b+c) / 3;
+            return promedioResultado;
+        }
+    }
+}

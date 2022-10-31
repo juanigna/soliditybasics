001c1c638ccbde0de47374475804c6ba0d4ff1b7
Juan Ignacio <jibocchi14@gmail.com>
2022-10-31 15:39:58 -0300

    Ejercicio 3

--- 3-argumentyfuncion.sol
+++ 3-argumentyfuncion.sol
@@ -2,4 +2,16 @@
 Tenes que crear un set () con un argument. Y setear el valor de la state variable creada con el valor pasado como argument al set().
 Luego, crea otra funcion get() que devuelva el valor de la state variable. */
 
+pragma solidity ^0.8.6;
 
+contract Ejercicio3{
+    uint public stateVariable;
+
+    function  set(uint _stateVariable) public{
+        stateVariable = _stateVariable;
+    }
+
+    function get() public view returns(uint){
+        return stateVariable;
+    }
+}

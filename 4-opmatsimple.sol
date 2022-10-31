bcaafd3fd894e16ecfbb2db3b9c2cf18d8f2b969
Juan Ignacio <jibocchi14@gmail.com>
2022-10-31 15:42:28 -0300

    Ejercicio 4

--- 4-opmatsimple.sol
+++ 4-opmatsimple.sol
@@ -2,3 +2,13 @@
 1) Crea una funcion evaluar(int a, int b).
 2) Resta la diferencia de a y b desde la suma de a y b.
 3) Devolve el resultado de la tarea anterior desde evaluar(). */
+
+pragma solidity ^0.8.6;
+
+contract Ejercicio3{
+
+    function evaluar(uint a, uint b) public view returns(uint){
+        uint suma = a + b;
+        return suma;
+    }
+}
